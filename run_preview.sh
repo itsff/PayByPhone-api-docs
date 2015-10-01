da67f58fbd3f4e2ea81927b20eb0c96640e63c0a
Matheus Felipe <matbhz@gmail.com>
2015-10-01 11:45:31 -0700

    Some text changes to test

--- run_preview.sh
+++ run_preview.sh
@@ -1,10 +1,11 @@
 APIARY_FILE=apiary.apib
+APIARY_MANIFEST_FILE=apiary.manifest 
 
 # Clear apiary.apib file
 > $APIARY_FILE
 
 # Merges each file defined in the manifest to the new apiary.apib file
-cat apiary.manifest | \
+cat $APIARY_MANIFEST_FILE | \
 while read CMD; do
 	echo "Merging " $CMD
 	cat $CMD >> $APIARY_FILE; 
